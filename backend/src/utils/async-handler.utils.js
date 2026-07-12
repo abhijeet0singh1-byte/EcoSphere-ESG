b9c6a691a66ecabb281124f3979ebf0271245122
Adi <luffy1120adi@gmail.com>
2026-07-12 11:29:29 +0530

    Added utils

---
+++ backend/src/utils/async-handler.utils.js
@@ -0,0 +1,7 @@
+const asyncHandler = (asyncRequest) => {
+  return (req, res, next) => {
+    Promise.resolve(asyncRequest(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
